da89f9a2b60f280379daa37d524e2c3da40d40b3
bishnulimbu <subashlimbu987654321@gmail.com>
2024-03-11 21:21:06 +0545

    description box

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>A home decor website is your one-stop shop for transforming your living space into a stylish and inviting haven. Browse a curated selection of furniture, decor pieces, and accessories to find the perfect fit for any room. Whether you're aiming for a modern minimalist look or a cozy traditional vibe, they'll help you create a home that reflects your unique style. Many sites also offer inspirational ideas and expert advice to guide you on your decorating journey.</p>
+        <p>The Sustainable Haven: Decorate your home with eco-conscious style. This website offers a wide range of eco-friendly decor made from recycled materials and sustainable practices. Find beautiful pieces that are good for your home and the planet.
+          The DIY Den: Unleash your inner interior designer! This website provides all the inspiration and resources you need to tackle DIY home decor projects. Find tutorials, product recommendations, and creative ideas to personalize your space on a budget.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default DescriptionBox
