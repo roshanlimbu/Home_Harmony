c09134639d6231f210e477d61418a895c5c83a28
roshanlimbu <birenlimbu415@gmail.com>
2024-03-12 22:24:09 +0545

    ikd why earlier code did not run now it's running like machine gun

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
 const express = require("express");
-require('dotenv').config();
-const PORT = 4000;
-const app = express();
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const multer = require("multer");
-const path = require("path");
+require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+const app = express();
+const PORT = process.env.PORT || 7000;
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+
+mongoose
+  .connect(
+    "mongodb+srv://birenlimbu415:JVZc0AZmwXeA34Cp@cluster01.dee5ny7.mongodb.net/e-commerce",
+  )
+  .catch((error) => {
+    console.error("An error occurred while connecting to mongodb", error);
+  });
+
+app.get("/", (req, res) => {
+  res.send({ msg: "hello" });
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(`Error starting server`);
+  } else {
+    console.log(`Server started at ${PORT}`);
+  }
+});
