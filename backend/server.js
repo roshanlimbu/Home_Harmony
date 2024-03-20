01c8e62fa38f98a155214e5c6588c77f0560105e
roshanlimbu <birenlimbu415@gmail.com>
2024-03-20 15:04:08 +0545

    added error handling
    Added error handling so that postman does not play holi with me

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,14 @@ const upload = multer({ storage: storage });
 app.use("/images", express.static("upload/images"));
 
 app.post("/upload", upload.single("product"), (req, res) => {
-  res.json({
-    success: 1,
-    image_url: `http:localhost:${PORT}/images/${req.file.filename}`,
-  });
+  if (!req.file) {
+    return res.status(400).json({ success: 0, message: "No file uploaded" });
+  } else {
+    res.json({
+      success: 1,
+      image_url: `http:localhost:${PORT}/images/${req.file.filename}`,
+    });
+  }
 });
 
 app.listen(PORT, (err) => {
