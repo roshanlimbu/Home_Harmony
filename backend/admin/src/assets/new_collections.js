eb6675b2d6a9217710c756b9e73e6181ed8d8837
roshanlimbu <birenlimbu415@gmail.com>
2024-05-08 13:46:39 +0545

    assets for amin panel

---
+++ backend/admin/src/assets/new_collections.js
@@ -0,0 +1,53 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "product_12",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "product_35",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "product_14",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 15,
+    name: "product_15",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "product_2",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 28,
+    name: "product_28",
+    image: p8_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default new_collections;
