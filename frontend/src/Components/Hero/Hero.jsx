221b5b2d635cdb5b7f0e0c7286eeb541cb3a4d62
roshanlimbu <birenlimbu415@gmail.com>
2024-05-08 23:03:11 +0545

    almost finished

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import "./Hero.css";
-import hand_icon from "../Assets/hand_icon.png";
-import arrow_icon from "../Assets/arrow.png";
-import hero_image from "../Assets/hero_image.png";
 
 const Hero = () => {
   return (
     <div className="hero">
-
       <div className="hero-main">
         <h2>NEW YEARS SALES</h2>
-        <h1>Up to <span>50</span>% off</h1>
+        <h1>
+          Up to <span>50</span>% off
+        </h1>
         <h2>HUNDREDS OF STYLES AVAILABLE</h2>
-        {/* <div> */}
-        {/*   <div className="hero-hand-icon"> */}
-        {/*     <p>new</p> */}
-        {/*     <img src={hand_icon} alt="" /> */}
-        {/*   </div> */}
-        {/*   <p>collections</p> */}
-        {/*   <p>for everyone</p> */}
-        {/* </div> */}
-        {/* <div className="hero-latest-btn"> */}
-        {/*   <div>latest Collection</div> */}
-        {/*   <img src={arrow_icon} alt="" /> */}
-        {/* </div> */}
       </div>
     </div>
   );
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,73 @@
 import { useState, useRef, useContext } from "react";
 import React from "react";
 import "./Navbar.css";
-// import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
-import nav_dropdown from '../Assets/nav_dropdown.png'
+import nav_dropdown from "../Assets/nav_dropdown.png";
+import logo from "../Assets/nav-logo.png";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
   const menuRef = useRef();
   const dropdown_toggle = (e) => {
-    menuRef.current.classList.toggle('nav-menu-visible');
-    e.target.classList.toggle('open');
-  }
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
 
   return (
     <div className="navbar">
       <div className="nav-logo">
         <Link to="/" style={{ textDecoration: "none" }}>
-          {/* <img src={logo} alt="" /> */}
-          <p>
-            Home <span>Harmony</span>
-          </p>
+          <img src={logo} alt="" />
         </Link>
       </div>
-      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      <img
+        className="nav-dropdown"
+        onClick={dropdown_toggle}
+        src={nav_dropdown}
+        alt=""
+      />
       <ul ref={menuRef} className="nav-menu">
-        <li onClick={() => { setMenu("shop"); }}>
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
           <Link style={{ textDecoration: "none", color: "black" }} to="/">
             Furniture
           </Link>
           {menu === "shop" ? <hr /> : null}
         </li>
-        <li onClick={() => { setMenu("office"); }}>
+        <li
+          onClick={() => {
+            setMenu("office");
+          }}
+        >
           <Link style={{ textDecoration: "none", color: "black" }} to="office">
             Office
           </Link>
           {menu === "office" ? <hr /> : null}
         </li>
-        <li onClick={() => { setMenu("Kitchenware"); }}>
-          <Link style={{ textDecoration: "none", color: "black" }} to="Kitchenware">
+        <li
+          onClick={() => {
+            setMenu("Kitchenware");
+          }}
+        >
+          <Link
+            style={{ textDecoration: "none", color: "black" }}
+            to="Kitchenware"
+          >
             Kitchenware
           </Link>
           {menu === "Kitchenware" ? <hr /> : null}
         </li>
-        <li onClick={() => { setMenu("decor"); }}>
+        <li
+          onClick={() => {
+            setMenu("decor");
+          }}
+        >
           <Link style={{ textDecoration: "none", color: "black" }} to="/decor">
             Decor
           </Link>
@@ -54,9 +75,20 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="nav-login-cart">
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
+        {localStorage.getItem("auth-token") ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("auth-token");
+              window.location.replace("/");
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        )}
         <Link to="/cart">
           <img src={cart_icon} alt="" />
         </Link>
