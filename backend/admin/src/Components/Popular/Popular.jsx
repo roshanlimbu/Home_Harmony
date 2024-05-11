cc8a1de8aa1ff7dbfb5442dac4e9c3d667a6a92f
roshanlimbu <birenlimbu415@gmail.com>
2024-05-12 01:21:31 +0545

    initialized popular.jsx

---
+++ backend/admin/src/Components/Popular/Popular.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Popular = () => {
+  return (
+    <div>
+      <h1>Hello this is popular section</h1>
+    </div>
+  );
+};
+
+export default Popular;
