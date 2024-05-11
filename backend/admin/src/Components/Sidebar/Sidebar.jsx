f6b696fae51340fba736269d8c1ce0477e92610d
roshanlimbu <birenlimbu415@gmail.com>
2024-05-11 23:29:50 +0545

    added sidebar for popular items

--- backend/admin/src/Components/Sidebar/Sidebar.jsx
+++ backend/admin/src/Components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import "./Sidebar.css";
 import { Link } from "react-router-dom";
 import add_product_icon from "../../assets/Product_Cart.svg";
 import list_product_icons from "../../assets/Product_list_icon.svg";
+import popularity from "../../assets/popularity.png";
 
 const Sidebar = () => {
   return (
@@ -17,6 +18,12 @@ const Sidebar = () => {
         <div className="sidebar-item">
           <img src={list_product_icons} />
           <p>Product List</p>
+        </div>
+      </Link>
+      <Link to="/popular" style={{ textDecoration: "none" }}>
+        <div className="sidebar-item">
+          <img src={popularity} />
+          <p>Popular Items</p>
         </div>
       </Link>
     </div>
