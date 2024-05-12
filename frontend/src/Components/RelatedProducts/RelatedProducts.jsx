4975d7f59bad40c586064275d92165b7c801fae7
roshanlimbu <birenlimbu415@gmail.com>
2024-05-13 00:28:58 +0545

    related products fixed

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import './RelatedProducts.css'
-import data_products from '../Assets/data'
-import Item from '../Item/Item'
+import React, { useEffet, useState } from "react";
+import "./RelatedProducts.css";
+import axios from "axios";
+import Item from "../Item/Item";
+import { useEffect } from "react";
 
 const RelatedProducts = () => {
+  const [relatedProduct, setRelatedProduct] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/popular/popular")
+      .then((response) => {
+        setRelatedProduct(response.data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <div className="relatedproducts">
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {data_products.map((item, i) => {
-          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        {relatedProduct.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
         })}
-
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default RelatedProducts 
+export default RelatedProducts;
