d70ae713d9e226fab53964b7b1d5ab5c8b4d72d6
roshanlimbu <birenlimbu415@gmail.com>
2024-05-12 23:33:30 +0545

    fixed show product and image

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -15,59 +15,72 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-    fetch("http://localhost:5000/allproducts")
-      .then((response) => response.json())
-      .then((data) => setAllProduct(data));
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/products/all-products",
+        );
+        const data = await response.json();
+        setAllProduct(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
   }, []);
 
-  const addToCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:5000/addtocart", {
-        method: "POST",
-        headers: {
-          Accept: "application/form-data",
-          "auth-token": `${localStorage.getItem("auth-token")}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ itemId: itemId }),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-    }
-  };
-  const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-  };
-  const getTotalCartAmount = () => {
-    let totalAmount = 0;
-    for (const item in cartItems) {
-      if (cartItems[item] > 0) {
-        let itemInfo = all_product.find(
-          (product) => product.id === Number(item),
-        );
-        totalAmount += itemInfo.new_price * cartItems[item];
-      }
-    }
-    return totalAmount;
-  };
-  const getTotalCartItems = () => {
-    let totalItem = 0;
-    for (const item in cartItems) {
-      if (cartItems[item] > 0) {
-        totalItem += cartItems[item];
-      }
-    }
-    return totalItem;
-  };
-
+  // console.log("all_product:", all_product);
+  // const addToCart = (itemId) => {
+  //   setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  //   if (localStorage.getItem("auth-token")) {
+  //     fetch("http://localhost:5000/addtocart", {
+  //       method: "POST",
+  //       headers: {
+  //         Accept: "application/form-data",
+  //         "auth-token": `${localStorage.getItem("auth-token")}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ itemId: itemId }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => console.log(data));
+  //   }
+  // };
+  // const removeFromCart = (itemId) => {
+  //   setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  // };
+  // const getTotalCartAmount = () => {
+  //   let totalAmount = 0;
+  //   if (all_product.length > 0) {
+  //     for (const item in cartItems) {
+  //       if (cartItems[item] > 0) {
+  //         let itemInfo = all_product.find(
+  //           (product) => product.id === Number(item),
+  //         );
+  //         totalAmount += itemInfo.new_price * cartItems[item];
+  //       }
+  //     }
+  //   }
+  //   return totalAmount;
+  // };
+  // const getTotalCartItems = () => {
+  //   let totalItem = 0;
+  //   for (const item in cartItems) {
+  //     if (cartItems[item] > 0) {
+  //       totalItem += cartItems[item];
+  //     }
+  //   }
+  //   return totalItem;
+  // };
+  //
   const contextValue = {
-    getTotalCartItems,
-    getTotalCartAmount,
     all_product,
-    cartItems,
-    addToCart,
-    removeFromCart,
+    // getTotalCartItems,
+    // getTotalCartAmount,
+    // cartItems,
+    // addToCart,
+    // removeFromCart,
   };
   return (
     <ShopContext.Provider value={contextValue}>
