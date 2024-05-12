2023fab717fb33ebfdf2d3061e891537f7650f54
roshanlimbu <birenlimbu415@gmail.com>
2024-05-12 21:38:03 +0545

    added route for new collection.jsx

--- backend/admin/src/Pages/Admin/Admin.jsx
+++ backend/admin/src/Pages/Admin/Admin.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import AddProduct from "../../Components/AddProduct/AddProduct";
 import ListProduct from "../../Components/ListProduct/ListProduct";
 import Popular from "../../Components/Popular/Popular";
+import NewCollection from "../../Components/NewCollection/NewCollection";
 
 const Admin = () => {
   return (
@@ -14,6 +15,10 @@ const Admin = () => {
         <Route path="/addproduct" element={<AddProduct></AddProduct>} />
         <Route path="/listproduct" element={<ListProduct></ListProduct>} />
         <Route path="/popular" element={<Popular></Popular>} />
+        <Route
+          path="/newCollection"
+          element={<NewCollection></NewCollection>}
+        />
       </Routes>
     </div>
   );
