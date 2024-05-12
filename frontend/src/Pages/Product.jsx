fb4bfba8528ea3a1353cd0046ec4c35c0f5cc28f
roshanlimbu <birenlimbu415@gmail.com>
2024-05-12 23:34:02 +0545

    idk what i fixed here but it's working now

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,26 +1,29 @@
-import React, { useContext } from 'react'
-import Breadcrum from '../Components/Breadcrums/Breadcrum';
-import { useParams } from 'react-router-dom';
-import { ShopContext } from '../Context/ShopContext';
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
-import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
-import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
+import React, { useContext } from "react";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
-  const product = all_product.find((e) => e.id === Number(productId))
+  // console.log(all_product.products); // it give the list of products array
+  const helperProduct = all_product.products; // needed idk why though
+
+  const product = helperProduct.find((e) => e.id === Number(productId));
+  if (!product || product === undefined) {
+    return <div>Product not found.</div>;
+  }
   return (
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
       <DescriptionBox />
       <RelatedProducts />
-
-
-
     </div>
-  )
-}
+  );
+};
 
 export default Product;
