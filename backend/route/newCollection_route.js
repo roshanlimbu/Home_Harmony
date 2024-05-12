ee6612fd76d40ba0fcf83386ec37d48f78b5b7bb
roshanlimbu <birenlimbu415@gmail.com>
2024-05-12 21:38:26 +0545

    route for newCollection

--- backend/route/newCollection_route.js
+++ backend/route/newCollection_route.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const { newCollection } = require("../controller/newCollection_controller");
 
-router.get("/newCollection", newCollection);
+router.get("/newcollection", newCollection);
 module.exports = router;
