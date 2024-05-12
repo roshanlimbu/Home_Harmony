90e10f1a3c7e71a385aac069edc47e813a23e376
roshanlimbu <birenlimbu415@gmail.com>
2024-05-13 00:29:13 +0545

    fixed error on reloading

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -9,13 +9,18 @@ import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 const Product = () => {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
-  // console.log(all_product.products); // it give the list of products array
-  const helperProduct = all_product.products; // needed idk why though
 
+  if (!all_product || !all_product.products) {
+    return <div>Loading...</div>; // or display a loading indicator
+  }
+
+  const helperProduct = all_product.products;
   const product = helperProduct.find((e) => e.id === Number(productId));
-  if (!product || product === undefined) {
+
+  if (!product) {
     return <div>Product not found.</div>;
   }
+
   return (
     <div>
       <Breadcrum product={product} />
