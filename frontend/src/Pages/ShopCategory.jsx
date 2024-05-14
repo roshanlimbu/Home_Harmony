a1e1f39ce74d2079eedea21489717529e18046db
roshanlimbu <birenlimbu415@gmail.com>
2024-05-14 22:10:35 +0545

    commented out dummy

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import "./CSS/ShopCategoty.css";
 import { ShopContext } from "../Context/ShopContext";
-import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+// import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/Item/Item";
 
 const ShopCategory = (props) => {
