98ef4bb777e517373aad9a0b4e1c87d767100e27
bishnulimbu <subashlimbu987654321@gmail.com>
2024-06-17 22:44:33 +0545

    same as before

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -14,9 +14,9 @@ const Item = (props) => {
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">${props.new_price}</div>
+        <div className="item-price-new">Rs. {props.new_price}</div>
       </div>
-      <div className="item-price-old">${props.old_price}</div>
+      <div className="item-price-old">Rs. {props.old_price}</div>
     </div>
   );
 };
